8287d93051f1592c7ed243089dbb64f6414287be
Danil_M <max.cacz@yandex.ru>
2023-01-15 20:26:38 +0500

    change main file

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,13 @@
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.coroutineScope
+import kotlin.coroutines.coroutineContext
 
 fun main() {
+
+    println("Hello World!")
+
+    val job = Job
+
 
 
 }
