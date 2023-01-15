70ab80d0d1df9cffa675feaeb6a2105d3178291d
Danil_M <max.cacz@yandex.ru>
2023-01-15 20:55:08 +0500

    add sample inline fun

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,10 +7,24 @@ fun main() {
 
     println("Hello World!")
 
+    val result = doSome { a, b -> a + b }
 
+    println(result)
 
 
 }
+
+
+inline fun doSome( lambda: (a: Int, b: Int) -> Int): Int = lambda.invoke(2,2)
+
+
+
+
+
+
+
+
+
 
 suspend fun getMovie() {}
 suspend fun getList() {}
