bdb7b620cc7f83c96f77f5747bc794255246a9d8
Danil_M <max.cacz@yandex.ru>
2023-01-17 03:12:22 +0500

    philosophy task

---
+++ src/main/kotlin/OldThread/Philosophy.java
@@ -0,0 +1,43 @@
+package OldThread;
+
+import java.util.concurrent.Semaphore;
+
+/**
+ Задача про обедающих философов
+ */
+public class Philosophy implements Runnable {
+
+    private Semaphore semaphore;
+    private boolean flag = false;
+
+    public Philosophy(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
+
+
+    @Override
+    public void run() {
+
+        try {
+            if (!flag) {
+
+                semaphore.acquire();
+
+                System.out.println("Сел есть");
+
+                Thread.sleep(300);
+                flag = true;
+
+                System.out.println("Поел");
+                semaphore.release();
+
+                Thread.sleep(300);
+            }
+        }
+        catch (InterruptedException exception) {
+            exception.printStackTrace();
+        }
+
+
+    }
+}
