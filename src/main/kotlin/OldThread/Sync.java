8ece3fc9f742db8883e5f493f095431859054fb9
Danil_M <max.cacz@yandex.ru>
2023-01-17 02:00:09 +0500

    sync class

--- src/main/kotlin/OldThread/Sync.java
+++ src/main/kotlin/OldThread/Sync.java
@@ -1,4 +1,23 @@
 package OldThread;
 
+/**
+ Для синхронизации используется ключевое слово synchronized
+ При работе внутри synchronized блока кода захватывается мьютекс переданного обьекта
+
+ */
+
 public class Sync {
+
+    private final int a = 0;
+    private Object object;
+
+
+    public void doSome() {
+
+        synchronized (object) {}
+
+    }
+
+    public synchronized void doSome2() {}
+
 }
