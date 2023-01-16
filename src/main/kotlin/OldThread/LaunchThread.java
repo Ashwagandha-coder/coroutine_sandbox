2bc5e408221fee18ebf197ca0199d3418db26632
Danil_M <max.cacz@yandex.ru>
2023-01-17 03:00:46 +0500

    volt and launch

--- src/main/kotlin/OldThread/LaunchThread.java
+++ src/main/kotlin/OldThread/LaunchThread.java
@@ -1,21 +1,12 @@
 package OldThread;
 
+
+/**
+ * Пример главного потока в Java
+ */
 public class LaunchThread {
 
-    public static void main(String[] args) {
-
-
-    }
-
-    public void doSome() {
-
-        Object object = new Object();
-
-        synchronized (object) {
-
-        }
-
-    }
+    public static void main(String[] args) {}
 
 
 }
--- src/main/kotlin/OldThread/Volt.java
+++ src/main/kotlin/OldThread/Volt.java
@@ -1,4 +1,18 @@
 package OldThread;
 
+/**
+ * Пример volatile в Java
+ */
 public class Volt {
+
+    private volatile int a = 0;
+
+    public void volt() {
+
+        a = 2;
+        a = 3;
+        a = 4;
+
+    }
+
 }
