187f8e7da7881b7b91552befa5212717c8bad099
Danil_M <max.cacz@yandex.ru>
2023-01-17 17:04:49 +0500

    add make method

--- src/main/kotlin/Executor/ExecutorTest.java
+++ src/main/kotlin/Executor/ExecutorTest.java
@@ -1,13 +1,33 @@
 package Executor;
 
 
+import kotlinx.coroutines.ExecutorCoroutineDispatcher;
+
 import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
 
 /**
  Пример использования Executor
  */
 public class ExecutorTest {
 
-    private Executor executor;
+    private Executor executor = (runnable_main) -> { new Thread(runnable_main).start();};
+
+    private ExecutorService executorService;
+    private ExecutorCompletionService executorCompletionService;
+    private ExecutorCoroutineDispatcher executorCoroutineDispatcher;
+
+
+    public void make() {
+
+        Runnable runnable = () -> {
+            System.out.println("Hello World");
+        };
+    }
+
 
 }
+
+
+
