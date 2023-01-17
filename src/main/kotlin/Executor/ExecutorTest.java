3079c16f3cc19a73ae0577396b4af9a57109b3ac
Danil_M <max.cacz@yandex.ru>
2023-01-17 17:21:26 +0500

    save

--- src/main/kotlin/Executor/ExecutorTest.java
+++ src/main/kotlin/Executor/ExecutorTest.java
@@ -2,7 +2,10 @@ package Executor;
 
 
 import kotlinx.coroutines.ExecutorCoroutineDispatcher;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.concurrent.Callable;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
@@ -14,9 +17,17 @@ public class ExecutorTest {
 
     private Executor executor = (runnable_main) -> { new Thread(runnable_main).start();};
 
+    private Callable callable;
+
     private ExecutorService executorService;
     private ExecutorCompletionService executorCompletionService;
     private ExecutorCoroutineDispatcher executorCoroutineDispatcher;
+
+    @NotNull
+    @Contract(pure = true)
+    private static Object call() {
+        return "Hello World";
+    }
 
 
     public void make() {
@@ -31,6 +42,7 @@ public class ExecutorTest {
 
     public void doMake() {
 
+        callable = ExecutorTest::call;
 
 
 
