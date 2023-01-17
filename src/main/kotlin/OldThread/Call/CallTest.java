662a04b9becde82f9b1bf671247092a529c93b35
Danil_M <max.cacz@yandex.ru>
2023-01-17 16:38:01 +0500

    [add] callable and future

---
+++ src/main/kotlin/OldThread/Call/CallTest.java
@@ -0,0 +1,19 @@
+package OldThread.Call;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.Future;
+
+/**
+ Пример интерфейса callable and future
+ */
+
+public class CallTest {
+
+    private Callable callable = () -> {
+        return "";
+    };
+
+    private Future future;
+
+
+}
