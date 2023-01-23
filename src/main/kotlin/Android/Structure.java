538d569d58facffd04822495ce39e17280cf80a3
Danil_M <max.cacz@yandex.ru>
2023-01-23 17:13:59 +0500

    [add] process

---
+++ src/main/kotlin/Android/Structure.java
@@ -0,0 +1,22 @@
+package Android;
+
+/**
+ Процессы в Android
+ */
+public class Structure {
+
+    /*
+
+    1. Foreground process
+    2. Visibility process
+    3. Service process
+    4. Cache process
+
+
+
+     */
+
+
+
+
+}
