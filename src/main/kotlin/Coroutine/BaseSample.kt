9e00f9f1fb00efdc79fdf499b5df9f8ae0417fd2
Danil_M <max.cacz@yandex.ru>
2023-01-23 20:26:41 +0500

    [add] javadoc above coroutine

--- src/main/kotlin/Coroutine/BaseSample.kt
+++ src/main/kotlin/Coroutine/BaseSample.kt
@@ -1,8 +1,13 @@
 package Coroutine
 
+import kotlin.coroutines.Continuation
+import kotlin.coroutines.resume
+
 class BaseSample {
 
     private val param: Int = 0
+
+    private lateinit var continuation: Continuation<Any>
 
 
     suspend fun getSome() {
@@ -19,6 +24,20 @@ class BaseSample {
     private fun herOOP(param: Int): Int {
 
         return param * 2
+
+    }
+
+    /**
+     Данный метод принимает continuation Интерфейс - который неявно принимают все suspend функции
+     В нем есть открытый api - это 2 методы и одно поле = context , resume, resumeWith
+     */
+
+
+    fun testContinuations(continuation: Continuation<Any>) {
+
+        continuation.context
+        continuation.resume(Any())
+//        continuation.resumeWith(Any)
 
     }
 
